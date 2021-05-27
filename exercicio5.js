d0407b9e27f2fd3b0cedb675737699f2a91ad488
Kamila Silva <kamillasilva140@gmail.com>
2021-05-26 23:43:42 -0300

    resposta corrigida

--- exercicio5.js
+++ exercicio5.js
@@ -4,11 +4,11 @@
 // b) Deverá ser possível passar 2 números para a operação escolhida
 // c) Deverá retornar o resultado e imprimir no console
 
-function somar( num1, num2){
+function somar( num1,num2){
     return num1 + num2
 }
 
-function multiplicar( num1,num2){
+function multiplicar(num1,num2){
     return num1 * num2
 }
 
@@ -20,28 +20,24 @@ function subtrair( num1, num2){
     return num1 - num2
 }
 
-let operaçao= 'multiplicaçao'
-let num1= 15
-let num2= 27
+let operaçao= 'site'
+let num1= 12
+let num2= 21
 
-switch (operaçao){
+switch (operaçao) {
     case 'soma':
         console.log(`O resultado da soma é = ${(somar (num1,num2))}`)
     break
-
     case 'multiplicaçao':
         console.log(`O resultado da multiplicaçao é = ${(multiplicar (num1,num2))}`)
     break
-
     case 'divisao':
         console.log(`O resultado da divisao é = ${dividir (num1,num2)}`)
-        break
-
+    break
     case 'subtraçao':
         console.log(`O resultado da subtraçao é = ${subtrair (num1, num2)}`)
-        break
-       defeault
-        console.log('Insira uma entrada válida')
-
+     break 
+     default:
+         console.log("Insira uma operação válida")
 
 }
