edeeef46ce24a044ad5413c9e16aca3db564f552
Kamila Silva <kamillasilva140@gmail.com>
2021-05-26 23:43:08 -0300

    resposta corrigida

--- exercicio3.js
+++ exercicio3.js
@@ -1,23 +1,21 @@
 //3 - Crie uma função que recebe 2 parâmetros e retorna o resultado da divisão entre eles. Além disso, se o resto dessa divisão for zero deverá imprimir no console o valor e dizer que ele é par.
 
 function divisao(num1,num2){
-  if( num1 % num2 ===0){
-    console.log("O numero é par")
-
-    return num1/num2
-  }
  
-  
-} 
-    console.log("O resultado é ", divisao(16,8))
+  if(num1/num2%2 ===0){
+    console.log("O numero é par")
+    return num1/ num2
+  }
+   else{
+    console.log("erro")
+  }
+ } console.log("O resultado é ", divisao(16,8))
 
+ 
+    
+ 
    
 
  
 
     
-    
-
-
-
-
